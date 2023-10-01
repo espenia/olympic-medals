ee5e0fa6f56c31dc88ad659092f6483274edbd7d
ramaxx95 <ramilozano@live.com.ar>
2023-10-01 19:36:02 -0300

    Agrego clase Eventos + tests

---
+++ src/main/java/com/tdd/grupo5/medallero/entities/Event.java
@@ -0,0 +1,51 @@
+package com.tdd.grupo5.medallero.entities;
+
+import lombok.Getter;
+
+import java.util.*;
+
+
+public class Event {
+
+    private int number_of_participants;
+    @Getter
+    private List<Person> participants;
+    private boolean open_status;
+
+    public Event(){
+
+        this.number_of_participants = 0;
+        this.participants = new ArrayList<Person>();
+        this.open_status = true;
+
+    }
+
+    public int getNumberOfParticipants(){
+
+        return this.number_of_participants;
+
+    }
+
+    public void addParticipant(Person participant){
+
+        if(this.open_status){
+            this.participants.add(participant);
+            this.number_of_participants++;
+        }
+
+    }
+
+    //Returns if the event is still active
+    public boolean isOpen() {
+
+        return this.open_status;
+
+    }
+
+    public void finishEvent() {
+
+        this.open_status = false;
+
+    }
+
+}
---
+++ src/test/java/com/tdd/grupo5/medallero/EventosTests.java
@@ -0,0 +1,51 @@
+package com.tdd.grupo5.medallero;
+
+import com.tdd.grupo5.medallero.entities.Event;
+import com.tdd.grupo5.medallero.entities.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EventosTests {
+
+    Event event = new Event();
+    Person participant = new Person("Michael Phelps", 1985L);
+
+
+    @Test
+    void test01CreatedEventHasZeroParticipants() {
+        assert event.getNumberOfParticipants() == 0;
+    }
+
+    @Test
+    void test02AddingAParticipantUpdatesTheNumberOfParticipantsSubscribed() {
+        event.addParticipant(participant);
+        assert event.getNumberOfParticipants() == 1;
+    }
+
+    @Test
+    void test03CreatingAnEventGivesItAnOpenStatus() {
+        assert event.isOpen();
+    }
+
+    @Test
+    void test04EndingAnEventChangesItsOpenStatus() {
+        event.finishEvent();
+        assert !event.isOpen();
+    }
+
+    @Test
+    void test05AddingAParticipantToAClosedEventDoesntChangeTheNumberOfParticipants() {
+        event.finishEvent();
+        event.addParticipant(participant);
+        assert event.getNumberOfParticipants() == 0;
+    }
+
+    @Test
+    void test06ListOfParticipantsIsNotNullAfterAddingANewParticipant() {
+        event.addParticipant(participant);
+        assert !event.getParticipants().isEmpty();
+    }
+
+}
